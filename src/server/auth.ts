4c2f27faaff6a83d5cfa1029e19b88a7d837a63e
Ángel Vega <angelantoniovega@gmail.com>
2024-08-10 09:21:17 -0500

    feat: update import path for env in auth.ts

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-import { env } from "@components/env";
+import { env } from "./../env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
